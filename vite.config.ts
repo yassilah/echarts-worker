f9f886c11302de498fe8e0e75ed9304ec4d49099
Yasser Lahbibi <yasser.lahbibi@apenhet.com>
2023-06-11 17:22:48 +0200

    fix: exports/types

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
     build: {
@@ -10,5 +11,6 @@ export default defineConfig({
     },
     worker: {
         format: 'es'
-    }
+    },
+    plugins: [dts()]
 })
